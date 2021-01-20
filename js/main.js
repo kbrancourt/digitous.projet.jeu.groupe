81c6af7112a0e72345a1561d211b1609ca1a9d02
Mgoumane <goumane.mahmoudou@gmail.com>
2021-01-20 15:39:50 +0100

    je passe la main

--- js/main.js
+++ js/main.js
@@ -1,46 +1,63 @@
-
 var images = ["désert.jpg", "forêt.jpg", "plage.jpg", "Levée-soleil.jpg"];
-
-
-
-
-function userClick() {
-
-  console.log("faceCard show")
-  $("#faceCard-1").show();
-  $("#backCard-1").css("display", "none");
-
-
+//mahmoudou
+// $(document).ready(function(){
+//     //qd je clique sur l'arrière d'une carte
+//     function userClick(){
+//       $(".backCard").click({
+//         //la photo apparait et disparait qd la souris n'est plus dessus
+//         function(){
+//             $("#backCard-1").css("display", "none");  
+//           },
+//         function(){
+//             $("#faceCard-1").css("display", "block");
+//           }, 
+//     })
+//     }
+// })
+// userClick()
+// ahmed
+var click = 0;
+function userClick1(n) {
+  $(`#faceCard-${n}`).css("display", "block");
+  $(`#backCard-${n}`).css("display", "none");
+  click++;
+  console.log("click",click)
 }
-
+function essaie() {
+  for (i = 1; i <= 6; i++) {
+    userClick1(i)
+  }
+}
 $(document).ready(function () {
   console.log("backCard")
   $("#backCard-1").click(function () {
-    userClick()
-  })
-
-})
-
-function machCard() {
-  var click = [];
-  for (i = 0; i > click.length; i++) {
-      if( === 2){
-        
+    userClick1(1)
+    if (click == 2) {
+      console.log($("#faceCard-5").attr("display"))
+      if ($("#faceCard-5").attr("display") == "block") {
+        $("#faceCard-1").css("display", "block");
+        $("#faceCard-5").css("display", "block");
+        console.log("ok")
+        click = 0; 
+      } else {
+        $("#faceCard-1").css("display", "none");
+        console.log("non")
       }
-  }
-}
-
-//var click = 0;
-//si l'utilisateur clique sur une photo: var click = 1
-//si l'utilisateur clique sur une 2ème photo: var click = 2
-
- //comparer la valeur de la nouvelle class avec la class de l'autre photo
- //if(class photo1 === class photo2{
-  // $(".photo1").css ("display", "block")
- //}) 
-// avec les 6 photos, faire 3 conditions
-//if(class photo2 === class photo3{
-  // $(".photo2").css ("display", "block")
- //})
-
-
+    }
+  })
+  $("#backCard-2").click(function () {
+    userClick1(2)
+  })
+  $("#backCard-3").click(function () {
+    userClick1(3)
+  })
+  $("#backCard-4").click(function () {
+    userClick1(4)
+  })
+  $("#backCard-5").click(function () {
+    userClick1(5)
+  })
+  $("#backCard-6").click(function () {
+    userClick1(6)
+  })
+})
