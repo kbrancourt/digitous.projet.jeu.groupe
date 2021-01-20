ca35e0ddf15291b40ece772f209155e30107b4a9
Mgoumane <goumane.mahmoudou@gmail.com>
2021-01-20 14:03:48 +0100

    étpae ahmed

--- js/main.js
+++ js/main.js
@@ -1,44 +1,34 @@
 
-var images = ["désert.jpg","forêt.jpg","plage.jpg","Levée-soleil.jpg"];
+var images = ["désert.jpg", "forêt.jpg", "plage.jpg", "Levée-soleil.jpg"];
 
 
-//mahmoudou
-// $(document).ready(function(){
-//     //qd je clique sur l'arrière d'une carte
 
-//     function userClick(){
-//       $(".backCard").click({
-//         //la photo apparait et disparait qd la souris n'est plus dessus
-//         function(){
-           
-//             $("#backCard-1").css("display", "none");  
-//           },
-        
-//         function(){
-//             $("#faceCard-1").css("display", "block");
-//           }, 
-//     })
-//     }
-    
-// })
-// userClick()
 
-// ahmed
+function userClick() {
 
-function userClick(){
   console.log("faceCard show")
   $("#faceCard-1").show();
   $("#backCard-1").css("display", "none");
+
+
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
   console.log("backCard")
-  $("#backCard-1").click(function(){
+  $("#backCard-1").click(function () {
     userClick()
   })
 
 })
 
+function machCard() {
+  var click = [];
+  for (i = 0; i > click.length; i++) {
+      if( === 2){
+        
+      }
+  }
+}
 
 //var click = 0;
 //si l'utilisateur clique sur une photo: var click = 1
