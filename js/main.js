75bcfd8fe023f0af83667cc65be3a660836c6761
Mgoumane <goumane.mahmoudou@gmail.com>
2021-01-21 15:11:06 +0100

    je passe la main

--- js/main.js
+++ js/main.js
@@ -27,109 +27,124 @@ function userClick1(n) {
 }
 
 
-$(document).ready(function() {
-        $("#backCard-1").click(function() {
-            userClick1(1)
-            if (click === 2 && ($("#faceCard-5").css("display") === "block") && ($("#faceCard-1").css("display") === "block")) {
-                $("#faceCard-1").css("display", "block");
-                $("#faceCard-5").css("display", "block");
-                console.log("ok1");
-                click = 0
+$(document).ready(function () {
+    $("#backCard-1").click(function () {
+        userClick1(1)
+        if (click === 2 && ($("#faceCard-5").css("display") === "block") && ($("#faceCard-1").css("display") === "block")) {
+            $("#faceCard-1").css("display", "block");
+            $("#faceCard-5").css("display", "block");
+            console.log("ok1");
+            click = 0
 
-            }
-            if(click === 2 && ($("#faceCard-2").css("display") === "block") && ($("#faceCard-1").css("display") === "block")){
+        }
+        if (click === 2 && ($("#faceCard-2").css("display") === "block") && ($("#faceCard-1").css("display") === "block")) {
 
-                // $("#faceCard-1").css("display", "none");
-                // $("#faceCard-2").css("display", "none");
+            // $("#faceCard-1").css("display", "none");
+            // $("#faceCard-2").css("display", "none");
 
-                $("#backCard-1").css("display", "block");
-                $("#backCard-2").css("display", "block");
-                console.log("no");
-                click = 0
+            /* $("#backCard-1").css("display", "block");
+             $("#backCard-2").css("display", "block");*/
+            $("#backCard-1").animate({display: "block"}, 2000)
+            $("#backCard-2").animate({display: "block"}, 2000)
+                
+            
+            $("#facecard-1").animate({ display: "none"}, 2000);
+            $("#facecard-2").animate({display: "none"}, 2000);
+            console.log("no");
+            click = 0
 
-            }
-        });
-
-
-        $("#backCard-2").click(function() {
-            userClick1(2)
-            if (click === 2 && ($("#faceCard-6").css("display") === "block") && ($("#faceCard-2").css("display") === "block")) {
-                $("#faceCard-2").css("display", "block")
-                $("#faceCard-6").css("display", "block")
-                console.log("ok2");
-                click = 0
+        }
+    });
 
 
-            }
-
-            if(click === 2 && ($("#faceCard-2").css("display") === "block") && ($("#faceCard-1").css("display") === "block")){
-
-                // $("#faceCard-1").css("display", "none");
-                // $("#faceCard-2").css("display", "none");
-
-                $("#backCard-1").css("display", "block");
-                $("#backCard-2").css("display", "block");
-                console.log("no");
-                click = 0
-
-            }
-        })
+    $("#backCard-2").click(function () {
+        userClick1(2)
+        if (click === 2 && ($("#faceCard-6").css("display") === "block") && ($("#faceCard-2").css("display") === "block")) {
+            $("#faceCard-2").css("display", "block")
+            $("#faceCard-6").css("display", "block")
+            console.log("ok2");
+            click = 0
 
 
-        $("#backCard-3").click(function() {
-            userClick1(3)
-            if (click === 2 && ($("#faceCard-3").css("display") === "block") && ($("#faceCard-4").css("display") === "block")) {
-                $("#faceCard-4").css("display", "block");
-                $("#faceCard-3").css("display", "block");
-                console.log("ok3");
-                click = 0
+        }
 
+        if (click === 2 && ($("#faceCard-2").css("display") === "block") && ($("#faceCard-1").css("display") === "block")) {
 
-            }
-        })
+            // $("#faceCard-1").css("display", "none");
+            // $("#faceCard-2").css("display", "none");
 
+            /* $("#backCard-1").css("display", "block");
+             $("#backCard-2").css("display", "block");
+             console.log("no");
+             click = 0*/
+            $("#backCard-1").animate({ display: "block"}, 2000);
+            $("#backCard-2").animate({display: "block"}, 2000);
 
-        $("#backCard-4").click(function() {
-            userClick1(4)
-            if (click === 2 && ($("#faceCard-3").css("display") === "block") && ($("#faceCard-4").css("display") === "block")) {
-                $("#faceCard-3").css("display", "block");
-                $("#faceCard-4").css("display", "block");
-                console.log("ok3");
-                click = 0
+            $("#facecard-1").animate({ display: "none"}, 2000);
+            $("#facecard-2").animate({display: "none"}, 2000);
+                
+            
+            console.log("no");
+            click = 0
 
-
-            }
-        })
-
-
-        $("#backCard-5").click(function() {
-            userClick1(5)
-            if (click === 2 && ($("#faceCard-5").css("display") === "block") && ($("#faceCard-1").css("display") === "block")) {
-                $("#faceCard-1").css("display", "block");
-                $("#faceCard-5").css("display", "block");
-                console.log("ok1");
-                click = 0
-
-
-            }
-        })
-
-
-
-
-        $("#backCard-6").click(function() {
-            userClick1(6)
-            if (click === 2 && ($("#faceCard-6").css("display") === "block") && ($("#faceCard-2").css("display") === "block")) {
-                $("#faceCard-2").css("display", "block");
-                $("#faceCard-6").css("display", "block");
-                console.log("ok2");
-                count = 0
-
-
-            }
-        })
-
+        }
     })
+
+
+    $("#backCard-3").click(function () {
+        userClick1(3)
+        if (click === 2 && ($("#faceCard-3").css("display") === "block") && ($("#faceCard-4").css("display") === "block")) {
+            $("#faceCard-4").css("display", "block");
+            $("#faceCard-3").css("display", "block");
+            console.log("ok3");
+            click = 0
+
+
+        }
+    })
+
+
+    $("#backCard-4").click(function () {
+        userClick1(4)
+        if (click === 2 && ($("#faceCard-3").css("display") === "block") && ($("#faceCard-4").css("display") === "block")) {
+            $("#faceCard-3").css("display", "block");
+            $("#faceCard-4").css("display", "block");
+            console.log("ok3");
+            click = 0
+
+
+        }
+    })
+
+
+    $("#backCard-5").click(function () {
+        userClick1(5)
+        if (click === 2 && ($("#faceCard-5").css("display") === "block") && ($("#faceCard-1").css("display") === "block")) {
+            $("#faceCard-1").css("display", "block");
+            $("#faceCard-5").css("display", "block");
+            console.log("ok1");
+            click = 0
+
+
+        }
+    })
+
+
+
+
+    $("#backCard-6").click(function () {
+        userClick1(6)
+        if (click === 2 && ($("#faceCard-6").css("display") === "block") && ($("#faceCard-2").css("display") === "block")) {
+            $("#faceCard-2").css("display", "block");
+            $("#faceCard-6").css("display", "block");
+            console.log("ok2");
+            count = 0
+
+
+        }
+    })
+
+})
     //si je clique 2 fois et que card 5 = card 1
     // if (click === 2 && ($("#faceCard-5").css("display") === "block") && ($("#faceCard-1").css("display") === "block")) {
     //     $("#faceCard-1").css("display", "block");
